f137a833dfb635a503000361c7f45d813ebb4fcf
Lappy000 <not.furry10@gmail.com>
2026-05-21 14:10:00 +0300

    chore: extract version info to dedicated module

---
+++ src/lockfile_lint/version.py
@@ -0,0 +1,10 @@
+"""Version management."""
+__version__ = "0.1.0"
+
+def get_version_info() -> dict:
+    """Return version metadata."""
+    return {
+        "version": __version__,
+        "python_minimum": "3.9",
+        "malicious_db_version": "2026.05.20",
+    }
